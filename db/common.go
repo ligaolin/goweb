4c1e6f01ef9f604912439e1147a18024f7d75fd2
lin <976791636@qq.com>
2025-10-05 09:27:08 +0800

    update

--- db/common.go
+++ db/common.go
@@ -16,61 +16,41 @@ type FirstParam struct {
 }
 
 type ListParamBase struct {
-	Page     int    `form:"page"`
-	PageSize int    `form:"page_size"`
+	Page     int32  `form:"page"`
+	PageSize int32  `form:"page_size"`
 	Order    string `form:"order"`
 }
 
-// 模型基础字段
-type IDCreatedAtUpdatedAt struct {
-	ID        int32 `json:"id" gorm:"primarykey;comment:ID"`
-	CreatedAt Time  `json:"created_at" gorm:"comment:创建时间"`
-	UpdatedAt Time  `json:"updated_at" gorm:"comment:更新时间"`
+type ModelID struct {
+	ID int32 `gorm:"column:id;primaryKey;type:autoIncrement;comment:ID" json:"id"`
 }
 
-// 模型基础字段
-type IDCreatedAtUpdatedAtDeletedAt struct {
-	IDCreatedAtUpdatedAt
-	DeletedAt Time `json:"deleted_at" gorm:"index;comment:删除时间"`
+type ModelCreatedAt struct {
+	CreatedAt Time `gorm:"column:created_at;type:datetime(3);autoCreateTime:milli;comment:创建时间" json:"created_at"`
 }
 
-// 模型排序
-type SortStruct struct {
-	Sort int32 `json:"sort" gorm:"type:int(11);default:100;comment:排序"`
+type ModelUpdatedAt struct {
+	UpdatedAt Time `gorm:"column:updated_at;type:datetime(3);autoUpdateTime:milli;comment:更新时间" json:"updated_at"`
 }
 
-// 模型状态
-type StateStruct struct {
-	State string `json:"state" gorm:"type:enum('开启','关闭');default:开启;comment:状态"`
+type ModelDeleteAt struct {
+	DeleteAt Time `gorm:"column:delete_at;type:datetime(3);comment:删除时间" json:"delete_at"`
 }
 
-// 模型排序和状态
-type SortState struct {
-	SortStruct
-	StateStruct
+type ModelSort struct {
+	Sort int32 `gorm:"column:sort;type:bigint(11);default:100;comment:排序" json:"sort"`
 }
 
-// 模型基础字段
-type IDCreatedAtUpdatedAtDeletedAtSortState struct {
-	IDCreatedAtUpdatedAtDeletedAt
-	SortState
+type ModelState struct {
+	State string `gorm:"column:state;type:enum('开启','关闭');default:'开启';comment:状态" json:"state"`
 }
 
-// 模型基础字段
-type IDCreatedAtUpdatedAtSortState struct {
-	IDCreatedAtUpdatedAt
-	SortState
+type ModelHasChildren struct {
+	HasChildren bool `gorm:"-:all;default:false" json:"hasChildren"`
 }
 
-type HasChildrenStruct struct {
-	HasChildren bool `json:"hasChildren" gorm:"-:all;default:false"`
-}
-
-type ChildrenStruct[T any] struct {
-	Children []T `json:"children" gorm:"-:all;"`
-}
-
-type PIDLevel struct {
-	PID   int32 `json:"pid" gorm:"column:pid;type:int(11);default:0;comment:父级id"`
-	Level int32 `json:"level" gorm:"type:int(2);default:1;comment:层级"`
+type ModelChildren[T any] struct {
+	PID      int32 `gorm:"column:pid;type:bigint(20);default:0;comment:父级id" json:"pid"`
+	Level    int32 `gorm:"column:level;type:tinyint(1);default:1;comment:层级" json:"level"`
+	Children []T   `gorm:"-:migration;foreignKey:pid;references:id" json:"children"`
 }
--- db/list.go
+++ db/list.go
@@ -1,15 +1,11 @@
 package db
 
-import (
-	"math"
-)
-
 type ListData struct {
 	Data      any   `json:"data"`
-	Total     int64 `json:"total"`      // 总数量
-	TotalPage int64 `json:"total_page"` // 总页数
-	Page      int   `json:"page"`
-	PageSize  int   `json:"page_size"`
+	Total     int32 `json:"total"`      // 总数量
+	TotalPage int32 `json:"total_page"` // 总页数
+	Page      int32 `json:"page"`
+	PageSize  int32 `json:"page_size"`
 }
 
 // 查询列表
@@ -19,18 +15,25 @@ func (m *Model) List(data *ListData) *Model {
 	}
 
 	// 查询总数
-	if err := m.Db.Model(m.Model).Count(&data.Total).Error; err != nil {
+	var total int64
+	if err := m.Db.Model(m.Model).Count(&total).Error; err != nil {
 		m.Error = err
 		return m
 	}
+	data.Total = int32(total)
 
 	// 添加分页
 	if data.Page > 0 {
-		if data.PageSize == 0 {
+		if data.PageSize <= 0 {
 			data.PageSize = 10
 		}
-		offset := (data.Page - 1) * data.PageSize
-		m.Db = m.Db.Limit(int(data.PageSize)).Offset(int(offset))
+		m.Db = m.Db.Offset(int((data.Page - 1) * data.PageSize))
+	}
+	if data.PageSize > 100 {
+		data.PageSize = 100
+	}
+	if data.PageSize > 0 {
+		m.Db = m.Db.Limit(int(data.PageSize))
 	}
 
 	// 执行查询
@@ -41,7 +44,7 @@ func (m *Model) List(data *ListData) *Model {
 
 	// 计算总页数
 	if data.Page > 0 {
-		data.TotalPage = int64(math.Ceil(float64(data.Total) / float64(data.PageSize)))
+		data.TotalPage = (data.Total + data.PageSize - 1) / data.PageSize
 	} else {
 		data.TotalPage = 1
 	}
