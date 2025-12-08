84b5b0a91bb709c8d4e974c87469d329ef9178f3
lin <976791636@qq.com>
2025-12-08 20:12:35 +0800

    update

--- response.go
+++ response.go
@@ -12,23 +12,13 @@ type Response struct {
 	Writer  http.ResponseWriter `json:"-"`
 }
 
-var res *Response
-
 func NewResponse(w http.ResponseWriter) *Response {
-	if res != nil {
-		res.Message = "success"
-		res.Code = 200
-		res.Data = nil
-		res.Writer = w
-	} else {
-		res = &Response{
-			Message: "success",
-			Code:    200,
-			Data:    nil,
-			Writer:  w,
-		}
+	return &Response{
+		Message: "success",
+		Code:    200,
+		Data:    nil,
+		Writer:  w,
 	}
-	return res
 }
 
 func (r *Response) Response() {
