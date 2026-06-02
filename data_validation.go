94b198caddd278d5885940f7dc21f95b840a3b4e
lin <976791636@qq.com>
2026-06-02 16:31:47 +0800

    update

--- data_validation.go
+++ data_validation.go
@@ -11,17 +11,23 @@ func IsNilOrZero(v any) bool {
 }
 
 func IsZero(v any) bool {
-	switch val := v.(type) {
-	case int, int8, int16, int32, int64:
-		return val == 0
-	case uint, uint8, uint16, uint32, uint64:
-		return val == 0
-	case string:
-		return val == ""
-	case nil:
-		return true
+	val := reflect.ValueOf(v)
+	switch val.Kind() {
+	case reflect.Pointer, reflect.Interface:
+		return val.IsNil()
+	case reflect.String:
+		return val.String() == ""
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return val.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return val.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return val.Float() == 0
+	case reflect.Bool:
+		return !val.Bool()
+	case reflect.Slice, reflect.Map, reflect.Array:
+		return val.Len() == 0
 	default:
-		// 其他类型用反射兜底
-		return reflect.ValueOf(v).IsZero() // Go 1.13+ 支持
+		return false
 	}
 }
